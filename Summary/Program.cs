af06a2349902e1bffbef73564cbdc3ae9399fdfa
Andrew <godygood@yandex.ru>
2022-12-04 20:23:24 +0300

    Summ random

--- Summary/Program.cs
+++ Summary/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 12;
-int numberB = 7;
-Console.WriteLine(numberA +numberB);
+﻿int numberA = new Random().Next(1, 100);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
