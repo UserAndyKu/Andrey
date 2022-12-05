56c397bb35150352c2a79f4de341de4eb5062745
Andrew <godygood@yandex.ru>
2022-12-05 18:14:33 +0300

    if/else

---
+++ Condition_If_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "лиза")
+    {
+        Console.WriteLine("Ура, это Лиза!!");
+    }
+    else
+    {
+        Console.Write("Привет, ");
+        Console.WriteLine(username);
+    }
