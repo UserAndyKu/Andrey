6bb52a66ded5f25f68c6a7b20464accf35f81ae1
Andrew <godygood@yandex.ru>
2022-12-08 19:50:54 +0300

    method_array

---
+++ MethodArray/Program.cs
@@ -0,0 +1,18 @@
+﻿int [] array = {12, 23, 331, 14, 53, 65, 71, 825, 9};
+
+int n = array.Length;
+int find = 825;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array [index] == find)
+    {
+        Console.WriteLine(index);
+        // Нахождение указанного элемента массива и остановка дальнейшего поиска
+        break; 
+    }
+    //index = index + 1;
+    index++;
+}
