136a46230a680182fe1bbf68ee8ab9b19b91937d
Andrew <godygood@yandex.ru>
2022-12-12 20:51:22 +0300

    поиск первой позиции массива

--- ArrayLibrary/Program.cs
+++ ArrayLibrary/Program.cs
@@ -1,16 +1,16 @@
-﻿void FillArray(int[] Collection)
+﻿void FillArray(int[] Collection)            //Заполнение массива методом войд
 
 {
-    int Length = Collection.Length;
+    int length = Collection.Length;
     int index = 0;
-    while (index > Length)
+    while (index < length)
     {
         Collection[index] = new Random().Next(1, 10);
         index++;
     }
 }
 
-void PrintArray(int[] col)
+void PrintArray(int[] col)                  //Вывод массива после заполнения 
 
 {
     int count = col.Length;
@@ -22,16 +22,17 @@ void PrintArray(int[] col)
     }
 }
 
-int IndexOf(int[] collection, int find)
+int IndexOf(int[] collection, int find)         //Поиск элемента массива 
 {
     int count = collection.Length;
     int index = 0;
-    int position = 0;
+    int position = -1;
     while (index < count)
     {
         if (collection [index] == find)
         {
             position = index;
+            break; 
         }
         index++;
     }
@@ -44,5 +45,5 @@ FillArray(array);
 PrintArray(array);
 Console.WriteLine();
 
-int pos = IndexOf(array, 4);
+int pos = IndexOf(array, 8);
 Console.WriteLine(pos);
