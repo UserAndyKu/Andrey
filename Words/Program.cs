819278bf0faf30bc4149ad90f81f937bfed4b592
Andrew <godygood@yandex.ru>
2022-12-26 22:29:29 +0300

    Words

---
+++ Words/Program.cs
@@ -0,0 +1,16 @@
+﻿int n = 0;
+
+void FindWords(string alphabet, char[] word, int lenght = 0)
+{
+    if (lenght == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[lenght] = alphabet[i];
+        FindWords(alphabet, word,  lenght + 1);
+    }
+}
+
+FindWords("аисв", new char[2]);
